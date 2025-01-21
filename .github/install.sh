6e92dabad7863acae15ccfb1795e0ba85bf9c6f2
Chấm <111698740+chamchamfy@users.noreply.github.com>
2025-01-21 09:31:55 +0700

    Update install.sh

--- .github/install.sh
+++ .github/install.sh
@@ -17,9 +17,10 @@ feature="$FEATURE"
 
 # khu vực fusion 
 Xem () { curl -s -L -G -N -H "$User" --connect-timeout 20 "$1"; }
-Taiver () { curl -s -L -H "$User" --connect-timeout 20 "$1" -o "$2"; }
-Taivek () { curl -S -k --connect-timeout 20 "$1" -o "$2"; }
-Taive () { curl -L -N -k -H "$User" --connect-timeout 20 "$1" -o "$2"; }
+Taivec () { curl -s -L -H "$User" --connect-timeout 20 "$1" -o "$2"; }
+Taiveck () { curl -S -k --connect-timeout 20 "$1" -o "$2"; }
+Taive() { wget "$1" -O "$2"; }
+Taivek() { wget --no-check-certificate "$1" -O "$2"; }
 XHex(){ xxd -p "$@" | tr -d "\n" | tr -d ' '; }
 ZHex(){ xxd -r -p "$@"; }
 apksign () { java -jar $HOME/.github/Tools/apksigner.jar sign --cert "$HOME/.github/Tools/testkey.x509.pem" --key "$HOME/.github/Tools/testkey.pk8" --out "$2" "$1"; }
